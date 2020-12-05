a1514addd7c74f8814ca5bdfda9279e7481fb074
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-12-06 00:42:11 +0500

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
+import {IconRoutingModule} from '../routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [
+    BrowserModule,
+    IconRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule,
+    MatButtonModule,
+    ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
