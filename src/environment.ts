d0413899500ba1ca60055c0b5f53c6010c2bbf2c
haziqAhmed7 <haziq.ahmed92@gmail.com>
2020-12-06 02:04:07 +0500

    3 files modified, 1 added

---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTsQPvW-DgpzDczcgmONre25Z-gOIdVUc",
+  authDomain: "icon-fumigation.firebaseapp.com",
+  databaseURL: "https://icon-fumigation-default-rtdb.firebaseio.com",
+  projectId: "icon-fumigation",
+  storageBucket: "icon-fumigation.appspot.com",
+  messagingSenderId: "569196247968",
+  appId: "1:569196247968:web:9202a43a4ed04dbe44c2c5",
+  measurementId: "G-RB2DWXZLRF"
+};
